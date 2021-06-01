69e8b1616847814d48ed36fe08cdd8186345b2f5
Firas Hasan <x.firashasan@gmail.com>
2021-06-01 07:23:57 +0300

    test lab7

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locationResult: [],
+      inputResult:'',
+
+    }
+
+  }
+
+  getWeather = async () => {
+    let serverRoute = process.env.REACT_APP_SERVER;
+    const url = `${serverRoute}/test`;
+    //we need to use axios to send it(request)
+    try {
+
+      const testData = await axios.get(url);
+      console.log(testData.data);
+    }
+    catch (error) {
+
+    }
+  }
+
+  saveInput=(event)=>{
+    
+    this.setState({
+      inputResult:event.target.value,
+
+    });
+    console.log(this.inputResult);
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1>CITY  EXPLORER </h1>
+        {/* <p>to send a request from here(local react app) to our backend(local server)
+              then connect them together</p> */}
+        {/* <button onClick={this.getWeather}>get weather stats</button> */}
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            {/* <Form.Label>city name</Form.Label> */}
+            <Form.Control type="text" placeholder="city name" onChange={this.saveInput}/>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+  </Button>
+        </Form>
+      </div>
+    )
+  }
 }
 
 export default App;
